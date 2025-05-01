53afba3b279df58617ec60874d9185910b41097e
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-01 22:25:32 +0200

    refactor: merge migrations

---
+++ migrations/Version20250501202441.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250501202441 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE article (id UUID NOT NULL, author_id UUID DEFAULT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, content TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, published_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, cover VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_23A0E66F675F31B ON article (author_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN article.id IS '(DC2Type:uuid)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN article.author_id IS '(DC2Type:uuid)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN article.created_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN article.updated_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN article.published_at IS '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE comment (id UUID NOT NULL, author_id UUID DEFAULT NULL, article_id UUID DEFAULT NULL, content TEXT NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_9474526CF675F31B ON comment (author_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_9474526C7294869C ON comment (article_id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN comment.id IS '(DC2Type:uuid)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN comment.author_id IS '(DC2Type:uuid)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN comment.article_id IS '(DC2Type:uuid)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)
+        SQL);
+        $this->addSql(<<<'SQL'
+            COMMENT ON COLUMN "user".id IS '(DC2Type:uuid)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE article ADD CONSTRAINT FK_23A0E66F675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE comment ADD CONSTRAINT FK_9474526CF675F31B FOREIGN KEY (author_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE comment ADD CONSTRAINT FK_9474526C7294869C FOREIGN KEY (article_id) REFERENCES article (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE article DROP CONSTRAINT FK_23A0E66F675F31B
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE comment DROP CONSTRAINT FK_9474526CF675F31B
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE comment DROP CONSTRAINT FK_9474526C7294869C
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE article
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE comment
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE "user"
+        SQL);
+    }
+}
