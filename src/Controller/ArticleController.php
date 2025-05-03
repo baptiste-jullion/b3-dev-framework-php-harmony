cbc61f4bb0fe5df8a27cfd491deb50e1888ebf92
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-05-03 15:23:45 +0200

    feat(Controllers): add 'edit' and 'delete' routes for Articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 
 use App\Entity\Article;
+use App\Form\ArticleForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 
 final class ArticleController extends AbstractController
@@ -15,7 +18,7 @@ final class ArticleController extends AbstractController
     #[Route('/blog', name: 'app_article_list')]
     public function list(EntityManagerInterface $entityManager): Response
     {
-        $articles = $entityManager->getRepository(Article::class)->findAll();
+        $articles = $entityManager->getRepository(Article::class)->findAllPublished();
 
         return $this->render('article/list.html.twig', [
             'articles' => $articles,
@@ -34,5 +37,49 @@ final class ArticleController extends AbstractController
         return $this->render('article/slug.html.twig', [
             'article' => $article,
         ]);
+    }
+
+    #[Route('/blog/{id}/edit', name: 'app_article_edit')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, EntityManagerInterface $entityManager, string $id): Response
+    {
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        $form = $this->createForm(ArticleForm::class, $article);
+        $form->handleRequest($request);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Article not found');
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setUpdatedAt(new \DateTimeImmutable());
+            $entityManager->flush();
+            $this->addFlash('success', 'Article updated successfully.');
+
+            return $this->redirectToRoute('app_article_by_slug', ['slug' => $article->getSlug()]);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/blog/{id}/delete', name: 'app_article_delete')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(EntityManagerInterface $entityManager, string $id): Response
+    {
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Article not found');
+        }
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+        $this->addFlash('success', 'Article supprimé.');
+
+        return $this->redirectToRoute('app_article_list');
     }
 }
